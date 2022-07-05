34f7bc11eb4fa165a0682fee1292c75c047b22a7
ingrid <ingridgeraldine31@gmail.com>
2022-07-05 17:03:18 -0500

    api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,18 @@
 import { Component } from '@angular/core';
+//import {HttpClient, HttpClientModule} from '@angular/common/http';
+import { HTTP } from '@awesome-cordova-plugins/http/ngx';
+
+import {Observable} from 'rxjs';
+import {NavController} from '@ionic/angular';
+import {HttpClient} from '@angular/common/http';
+
+const allowedOrigins = [
+  'capacitor://localhost',
+  'ionic://localhost',
+  'http://localhost',
+  'http://localhost:8080',
+  'http://localhost:8100',
+];
 
 @Component({
   selector: 'app-home',
@@ -7,6 +21,15 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  peliculas: Observable<any>;
+
+  //constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+  constructor(public navCtrl: NavController, public httpClient: HttpClient) {
+    this.peliculas = this.httpClient.get('http://swapi.co/api/films');
+    this.peliculas
+      .subscribe(data => {
+        console.log('my data: ', data);
+      });
+  }
 
 }
