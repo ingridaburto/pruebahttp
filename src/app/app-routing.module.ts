079b6d46ace013638514f8512d18147758eb08b7
ingrid <ingridgeraldine31@gmail.com>
2022-07-05 17:03:18 -0500

    api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'pelicula',
+    loadChildren: () => import('./pages/pelicula/pelicula.module').then( m => m.PeliculaPageModule)
+  },
 ];
 
 @NgModule({
